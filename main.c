0584dfadddf26cc22e1c60eedfe6d21b1aa8803c
HanRachid <handaouirachid@gmail.com>
2023-05-25 17:24:22 +0100

    fix EOD

--- main.c
+++ main.c
@@ -5,6 +5,7 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <signal.h>
 #include "main.h"
 
 /**
@@ -16,9 +17,10 @@
  * Return: void;
  */
 
-void execute(char *command, char *av, char **env, int count)
+int execute(char *command, char *av, char **env, int count)
 {
 	char **argv;
+	char cwd[1024];
 
 	argv = strtokarray(command, " \n\r\t");
 	argv[0] = strtok(argv[0], " \n\r\t");
@@ -26,22 +28,28 @@ void execute(char *command, char *av, char **env, int count)
 	if (strcmp(argv[0], "(null)") != 0)
 	{
 		if (strcmp(argv[0], "env") == 0)
+			execve("/bin/env", argv, env);
+		else if (strcmp(argv[0], "pwd") == 0)
 		{
-			if (execve("/bin/env", argv, env) == -1)
-			{
-				printf("%s: %d: %s: not found\n", av, count, argv[0]);
-				exit(EXIT_FAILURE);
-			}
+			if (getcwd(cwd, sizeof(cwd)) != NULL)
+				printf("%s\n", cwd);
+			else
+				perror("getcwd() error");
+		}
+		else if (strcmp(argv[0], "exit") == 0)
+		{
+			return (1);
 		}
 		else
 		{
 			if (execve(argv[0], argv, env) == -1)
 			{
 				printf("%s: %d: %s: not found\n", av, count, argv[0]);
-				exit(EXIT_FAILURE);
+				return (0);
 			}
 		}
 	}
+	return (0);
 }
 /**
  * shell_interactive - starts shell
@@ -56,39 +64,57 @@ void shell_interactive(char *av, char **env)
 	size_t size = 0;
 	pid_t child;
 	int count = 0;
-	char cwd[1024];
-	int checkpwd, checkexit, checknexit, status;
+	int status;
 
 	printf("($) ");
-	while (getline(&line, &size, stdin) != -1)
+	while (getline(&line, &size, stdin) != 0)
 	{
-
-		checkexit = strcmp(line, "exit\n");
-		checknexit = strncmp(line, "exit ", 5);
-		checkpwd = strcmp(line, "pwd\n");
+		if (feof(stdin))
+		{
+			clearerr(stdin);
+			if (line[0] != '\0')
+			{
+				child = fork();
+				if (child == 0)
+				{
+					if (execute(line, av, env, count) == 1)
+						exit(1);
+					exit(0);
+				}
+				else
+				{
+					wait(&status);
+					if (WIFEXITED(status) && WEXITSTATUS(status) == 1)
+						exit(0);
+				}
+			}
+			else
+			{
+				putchar('\n');
+				exit(0);
+			}
+			printf("($) ");
+			continue;
+		}
 		count++;
 		child = fork();
-		if (checkexit == 0 || checknexit == 0)
-			break;
-		if (child == 0 && strcmp(line, "pwd\n") == 0)
+		if (child == 0)
 		{
-			if (getcwd(cwd, sizeof(cwd)) != NULL)
-				printf("%s\n", cwd);
-			else
-				perror("getcwd() error");
-		}
-
-		if (child == 0 && checkpwd != 0 && checkexit != 0)
-		{
-			execute(line, av, env, count);
-			break;
+			if (execute(line, av, env, count) == 1)
+				exit(1);
+			free(line);
+			exit(0);
 		}
 		else
 		{
 			wait(&status);
-			printf("($) ");
+			if (WIFEXITED(status) && WEXITSTATUS(status) == 1)
+				exit(0);
+			else
+				printf("($) ");
 		}
 	}
+	free(line);
 	exit(0);
 }
 
@@ -107,8 +133,9 @@ void shell_nonint(char *av, char **env)
 	int count = 0;
 	char cwd[1024];
 	int checkpwd, checkexit, checknexit, status;
+	ssize_t result;
 
-	while (getline(&line, &size, stdin) != -1)
+	while ((result = getline(&line, &size, stdin)) != -1)
 	{
 		checknexit = strncmp(line, "exit ", 5);
 		checkexit = strcmp(line, "exit\n");
@@ -128,13 +155,12 @@ void shell_nonint(char *av, char **env)
 		if (child == 0 && checkpwd != 0 && checkexit != 0)
 		{
 			execute(line, av, env, count);
-			break;
+			free(line);
+			exit(0);
 		}
 		else
 			wait(&status);
 	}
-	free(line);
-	exit(0);
 }
 
 /**
