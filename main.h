e4d65011aa1d532d05d6191743f7d6b3aecd0298
HanRachid <handaouirachid@gmail.com>
2023-05-25 17:24:22 +0100

    merge conflict

--- main.h
+++ main.h
@@ -5,5 +5,5 @@ char **strtokarray(char *str, char *delim);
 void _which(char *str);
 void shell_nonint(char *av, char **env);
 void shell_interactive(char *av, char **env);
-void execute(char *command, char *av, char **env, int count);
+int execute(char *command, char *av, char **env, int count);
 #endif
